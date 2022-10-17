5c2de428522ead514bb5cb9dd772ca47e542bc3e
3nt3 <gott@3nt3.de>
2022-10-17 06:10:32 +0200

    add list subdomain
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,38 @@ use serde::{Deserialize, Serialize};
 #[get("/")]
 async fn handle_everything(_req: HttpRequest) -> HttpResponse {
     let host = _req.headers().get("host").unwrap().to_str().unwrap();
-    println!("host: {:?}", host);
+    println!("Host: {:?}", host);
 
     let domain_parts: Vec<&str> = host.split('.').collect();
     let subdomain = domain_parts[0];
+
+    if domain_parts.len() < 3 {
+        // this should honestly never happen because nginx only routes subdomains
+        return HttpResponseBuilder::new(StatusCode::BAD_REQUEST).body("no subdomain");
+    }
 
     println!("subdomain: {:?}", subdomain);
 
     match read_shorthands() {
         Ok(shorthands) => {
-            println!("{:?}", shorthands);
-            if domain_parts.len() < 3 {
+            if subdomain == "list" {
                 return HttpResponse::Ok().json(shorthands);
             }
+
             match shorthands.iter().find(|&x| x.short == subdomain) {
                 Some(shorthand) => {
-                    println!("redirecting {} to {}", host, shorthand.long);
+                    println!("Redirecting {} to {}", host, shorthand.long);
                     return HttpResponseBuilder::new(StatusCode::FOUND)
                         .insert_header(("Location", shorthand.long.clone()))
-                        .body(format!("redirecting to: {}", shorthand.long));
+                        .body(format!("Redirecting to: {}", shorthand.long));
                 }
-                None => HttpResponseBuilder::new(StatusCode::NOT_FOUND).body("not found"),
+                None => HttpResponseBuilder::new(StatusCode::NOT_FOUND).body("Not found"),
             }
         }
         Err(err) => {
-            println!("failed getting shorthands: {err}");
+            println!("Failed getting shorthands: {err}");
             return HttpResponseBuilder::new(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(format!("internal server error: {}", err.to_string()));
+                .body(format!("Internal server error: {}", err.to_string()));
         }
     }
 }
@@ -47,8 +52,14 @@ struct Shorthand {
 
 fn read_shorthands() -> anyhow::Result<Vec<Shorthand>> {
     let contents = fs::read_to_string("./shorthands.json")?;
-    match serde_json::from_str(&contents) {
-        serde_json::Result::Ok(x) => anyhow::Ok(x),
+    match serde_json::from_str::<Vec<Shorthand>>(&contents) {
+        serde_json::Result::Ok(x) => {
+            // check for 'list' shorthand
+            match x.iter().find(|item| item.short == "list") {
+                Some(_) => return Err(anyhow::Error::msg("The shorthand 'list' is reserverd.")),
+                None => return anyhow::Ok(x),
+            }
+        }
         serde_json::Result::Err(err) => Err(anyhow::Error::msg(err.to_string())),
     }
 }
@@ -56,8 +67,8 @@ fn read_shorthands() -> anyhow::Result<Vec<Shorthand>> {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     match read_shorthands() {
-        Ok(_) => println!("shorthands seem to exist and are able to be read :)"),
-        Err(err) => panic!("error reading shorthands: {}", err),
+        Ok(_) => println!("Shorthands seem to exist and are able to be read :)"),
+        Err(err) => panic!("Error reading shorthands: {}", err),
     }
 
     HttpServer::new(|| App::new().service(handle_everything))
